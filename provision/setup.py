856b7d1aac7d021b93e8a253932726c627fbad06
Mandeep Dhami <dhami@noironetworks.com>
2017-05-18 07:08:43 -0700

    Updated version number to push to pypi

--- provision/setup.py
+++ provision/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='acc_provision',
-    version='0.2',
+    version='0.2.1',
     description='Tool to provision ACI for ACI Containers Controller',
     author="Cisco Systems, Inc.",
     author_email="apicapi@noironetworks.com",
