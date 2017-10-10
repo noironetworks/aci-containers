10508c660c800bb8c9f35c4284bb2b0f22f4df79
Rob Adams <readams@readams.net>
2017-10-10 15:31:48 -0700

    Bump provision version
    Signed-off-by: Rob Adams <readams@readams.net>

--- provision/setup.py
+++ provision/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='acc_provision',
-    version='1.0.6',
+    version='1.7.0',
     description='Tool to provision ACI for ACI Containers Controller',
     author="Cisco Systems, Inc.",
     author_email="apicapi@noironetworks.com",
