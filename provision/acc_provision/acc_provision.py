74560e7ee2011b9a23d660910553a38c2af799e3
Rob Adams <readams@readams.net>
2017-11-21 14:18:37 -0800

    Improve output for provision tool
    * Display command to apply yaml file
    * Add additional command to clean up any stale kubernetes objects
    
    Signed-off-by: Rob Adams <readams@readams.net>

--- provision/acc_provision/acc_provision.py
+++ provision/acc_provision/acc_provision.py
@@ -14,6 +14,8 @@ import struct
 import sys
 import yaml
 import uuid
+import copy
+import os.path
 
 from OpenSSL import crypto
 from apic_provision import Apic, ApicKubeConfig
@@ -88,6 +90,7 @@ FLAVORS = {
                 "use_openshift_cluster_role": True,
                 "use_cnideploy_initcontainer": True,
                 "allow_kube_api_default_epg": True,
+                "kubectl": "oc",
             },
             "aci_config": {
                 "vmm_domain": {
@@ -167,6 +170,7 @@ def config_default():
             "controller": "1.1.1.1",
             "use_netpol_annotation": True,
             "image_pull_policy": "Always",
+            "kubectl": "kubectl",
         },
         "registry": {
             "image_prefix": "noiro",
@@ -487,16 +491,39 @@ def generate_kube_yaml(config, output):
     env.filters['yaml_quote'] = yaml_quote
     template = env.get_template('aci-containers.yaml')
 
-    info("Using configuration label aci-containers-config-version=" +
-         str(config["registry"]["configuration_version"]))
+    kube_objects = [
+        "configmap", "secret","serviceaccount",
+        "daemonset","deployment","clusterrolebinding",
+        "clusterrole"
+    ]
+    if config["kube_config"].get("use_openshift_security_context_constraints",
+                                 False):
+        kube_objects.append("securitycontextconstraints")
 
-    if output:
+    if output and output != "/dev/null":
+        outname = output
+        applyname = output
         if output == "-":
-            info("Writing kubernetes infrastructure YAML to \"STDOUT\"")
-            template.stream(config=config).dump(sys.stdout)
-        elif output != "/dev/null":
-            info("Writing kubernetes infrastructure YAML to \"%s\"" % output)
-            template.stream(config=config).dump(output)
+            outname = "<stdout>"
+            applyname = "<filename>"
+            output = sys.stdout
+        else:
+            applyname = os.path.basename(output)
+
+        info("Using configuration label aci-containers-config-version=" +
+            str(config["registry"]["configuration_version"]))
+        info("Writing kubernetes infrastructure YAML to %s" % outname)
+        template.stream(config=config).dump(output)
+        info("Apply infrastructure YAML using:")
+        info("  %s apply -f %s" %
+             (config["kube_config"]["kubectl"], applyname))
+        info("  %s -n kube-system delete %s -l "
+             " 'aci-containers-config-version,"
+             "aci-containers-config-version notin (%s)'" %
+             (config["kube_config"]["kubectl"],
+              ",".join(kube_objects),
+              str(config["registry"]["configuration_version"])))
+
     return config
 
 
@@ -515,8 +542,10 @@ def generate_apic_config(config, prov_apic, apic_file):
     if prov_apic is not None:
         apic = get_apic(config)
         if prov_apic is True:
+            info("Provisioning configuration in APIC")
             apic.provision(apic_config, sync_login)
         if prov_apic is False:
+            info("Unprovisioning configuration in APIC")
             system_id = config["aci_config"]["system_id"]
             tenant = config["aci_config"]["vrf"]["tenant"]
             apic.unprovision(apic_config, system_id, tenant)
