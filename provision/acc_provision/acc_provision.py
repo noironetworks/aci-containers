555d6ae3131e4ae7bff9a83c91d4ce1a01b0b2a0
Rob Adams <readams@readams.net>
2018-03-05 16:33:13 -0800

    Bump OpFlex agent version number
    Signed-off-by: Rob Adams <readams@readams.net>

--- provision/acc_provision/acc_provision.py
+++ provision/acc_provision/acc_provision.py
@@ -38,14 +38,14 @@ VERSIONS = {
         "cnideploy_version": "1.6r15",
         "aci_containers_host_version": "1.6r15",
         "aci_containers_controller_version": "1.6r15",
-        "opflex_agent_version": "1.6r20",
+        "opflex_agent_version": "1.6r21",
         "openvswitch_version": "1.6r12",
     },
     "1.7": {
         "cnideploy_version": "1.7r86",
         "aci_containers_host_version": "1.7r86",
         "aci_containers_controller_version": "1.7r86",
-        "opflex_agent_version": "1.7r68",
+        "opflex_agent_version": "1.7r69",
         "openvswitch_version": "1.7r24",
     },
 }
