d581aeedd257effdbe241a2b55ac60858fe3311a
Rob Adams <readams@readams.net>
2018-03-07 17:36:18 -0800

    Bump OpFlex image versions.
    Signed-off-by: Rob Adams <readams@readams.net>

--- provision/acc_provision/acc_provision.py
+++ provision/acc_provision/acc_provision.py
@@ -37,7 +37,7 @@ VERSIONS = {
         "cnideploy_version": "1.6r15",
         "aci_containers_host_version": "1.6r15",
         "aci_containers_controller_version": "1.6r15",
-        "opflex_agent_version": "1.6r21",
+        "opflex_agent_version": "1.6r22",
         "openvswitch_version": "1.6r12",
     },
 }
