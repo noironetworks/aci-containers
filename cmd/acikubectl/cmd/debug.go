e183a67126c8e0d68fdcb5e4e2f0a0c9bbdde0b8
Ceridwen <ceridwen@users.noreply.github.com>
2018-05-01 13:35:12 -0700

    Add ability to pass context argument to acikubectl

--- cmd/acikubectl/cmd/debug.go
+++ cmd/acikubectl/cmd/debug.go
@@ -31,7 +31,7 @@ import (
 )
 
 func execKubectl(args []string, out io.Writer) error {
-	baseargs := []string{"--kubeconfig", kubeconfig}
+	baseargs := []string{"--kubeconfig", kubeconfig, "--context", context}
 	cmd := exec.Command("kubectl", append(baseargs, args...)...)
 
 	cmd.Stdout = out
