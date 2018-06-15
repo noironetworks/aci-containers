13f2392aac79690d4fa4c82472f96311a1dd116d
Mandeep Dhami <dhami@noironetworks.com>
2018-06-15 14:24:17 -0700

    Updated insname to have length as an arguments

--- provision/acc_provision/acc_provision.py
+++ provision/acc_provision/acc_provision.py
@@ -540,15 +540,15 @@ def config_validate(flavor_opts, config):
              else Raise(Exception("Invalid value: %s; "
                                   "Expected one of: {%s}" %
                                   (x, ','.join(y)))))))
-    isname = lambda x: (1 < len(x) < 32) & \
-        x[0].isalpha() & x.replace('_', '').isalnum() \
-        if x else Raise(Exception("Missing option"))
+    isname = lambda x: (1 < len(x[0]) < x[1]) and \
+        x[0][0].isalpha() and x[0].replace('_', '').isalnum() \
+        if x[0] else Raise(Exception("Missing option"))
     get = lambda t: functools.reduce(lambda x, y: x and x.get(y), t, config)
 
     checks = {
         # ACI config
         "aci_config/system_id": (get(("aci_config", "system_id")), required),
-        "aci_config/system_id/type": (get(("aci_config", "system_id")), isname),
+        "aci_config/system_id/type": (((get(("aci_config", "system_id"))), 32), isname),
         "aci_config/apic_host": (get(("aci_config", "apic_hosts")), required),
         "aci_config/aep": (get(("aci_config", "aep")), required),
         "aci_config/vrf/name": (get(("aci_config", "vrf", "name")), required),
