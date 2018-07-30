2c53a0d72bc3394a0706fff97150336795fabe2c
Mandeep Dhami <dhami@noironetworks.com>
2018-07-30 14:59:27 -0700

    Fixed 42b3c97, typos /Prot/Port/ and /Namc/Name/

--- provision/setup.py
+++ provision/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='acc_provision',
-    version='1.9.0',
+    version='1.9.3',
     description='Tool to provision ACI for ACI Containers Controller',
     author="Cisco Systems, Inc.",
     author_email="apicapi@noironetworks.com",
