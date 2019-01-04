07f46ad327c5cdd34cf63a674359ecbd68be1962
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2019-01-03 18:09:54 -0800

    Bump acc-provision to 1.9.9 and images to 43 and 80
    More specifically:
    
    image: noiro/aci-containers-controller:1.9r43
    image: noiro/aci-containers-host:1.9r43
    image: noiro/openvswitch:1.7r24
    image: noiro/opflex:1.9r80

--- provision/setup.py
+++ provision/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='acc_provision',
-    version='1.9.8',
+    version='1.9.9',
     description='Tool to provision ACI for ACI Containers Controller',
     author="Cisco Systems, Inc.",
     author_email="apicapi@noironetworks.com",
