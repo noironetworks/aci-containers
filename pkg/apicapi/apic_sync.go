dc8285e1fb70f6801a44fe3e5b7704bba8a871ef
Apoorva <apomitta@apoorva-vm1.local.lan>
2019-10-03 16:04:08 -0400

    Snat sync for when acc restarts
    Removed the 5 sec sleep time introduced in https://github.com/noironetworks/aci-containers/pull/350

--- pkg/apicapi/apic_sync.go
+++ pkg/apicapi/apic_sync.go
@@ -19,8 +19,6 @@ import (
 	"encoding/hex"
 	"fmt"
 	"sort"
-	"strings"
-	"time"
 
 	"github.com/Sirupsen/logrus"
 )
@@ -256,10 +254,6 @@ func prepareApicCache(parentDn string, obj ApicObject) {
 }
 
 func (conn *ApicConnection) fullSync() {
-	// Sleep for service sync up except for unit tests
-	if !strings.Contains(conn.apic[conn.apicIndex], "127.0.0.1") {
-		time.Sleep(5 * time.Second)
-	}
 	conn.log.Info("Starting APIC full sync")
 	var updates ApicSlice
 	var deletes []string
