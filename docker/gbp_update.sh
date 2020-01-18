fbdd2f6a65161c7a9b10bc690b34840431363062
Joji Mekkatt <jojimtt@gmail.com>
2020-01-17 16:04:10 -0800

    Change some default values

--- docker/gbp_update.sh
+++ docker/gbp_update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-DST=/usr/local/etc/opflex-server/policy.json
+DST=/usr/local/var/lib/opflex-server/policy.json
 POD=$(kubectl get pods -n kube-system --field-selector status.podIP=$1 -l name=aci-containers-host -o custom-columns=":metadata.name" | grep aci)
 kubectl cp /tmp/gen_policy.$1.json kube-system/$POD:$DST -c opflex-server
