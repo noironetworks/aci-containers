f657fdaf2337744a0d23eddb69e93a6c8421fccf
Kishore Jonnala <50598933+kishorejonnala@users.noreply.github.com>
2020-08-04 13:02:19 -0700

    Revert "Revert "Use filter-chain for SNAT PBR contract""

--- pkg/controller/services_test.go
+++ pkg/controller/services_test.go
@@ -392,7 +392,7 @@ func TestServiceAnnotation(t *testing.T) {
 	service1.ObjectMeta.Annotations[metadata.ServiceContractScopeAnnotation] = "global"
 	time.Sleep(2 * time.Second)
 	cont.fakeServiceSource.Modify(service1)
-	contract := apicContract(name, "common", graphName, "global")
+	contract := apicContract(name, "common", graphName, "global", false)
 	expected := map[string]apicapi.ApicSlice{
 		graphName: apicapi.PrepareApicSlice(apicapi.ApicSlice{twoNodeCluster,
 			graph}, "kube", graphName),
@@ -464,7 +464,7 @@ func TestServiceGraph(t *testing.T) {
 	})
 
 	extNet := apicExtNet(name, "common", "l3out", []string{"10.4.2.2"}, false, false)
-	contract := apicContract(name, "common", graphName, conScope)
+	contract := apicContract(name, "common", graphName, conScope, false)
 	rsCons := apicExtNetCons(name, "common", "l3out", "ext1")
 
 	filter := apicapi.NewVzFilter("common", name)
