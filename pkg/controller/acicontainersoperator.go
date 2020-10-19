c6d038d8bf84fb46a6ae19991e23ad8763d7cc57
ABHISHEK SHARMA <abhisheksharmay2k@gmail.com>
2020-10-19 11:39:19 -0700

    Added flavor for OSP 4.5 Openstack/ESX in ACI Operator

--- pkg/controller/acicontainersoperator.go
+++ pkg/controller/acicontainersoperator.go
@@ -86,7 +86,10 @@ var Version = map[string]bool{
 	"openshift-4.3":           true,
 	"cloud":                   true,
 	"openshift-4.4-esx":       true,
+	"openshift-4.5-esx":       true,
 	"openshift-4.4-openstack": true,
+	"openshift-4.5-openstack": true,
+
 }
 var Dnsoper = map[string]bool{
 	"openshift-4.3": true,
