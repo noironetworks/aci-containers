34ee12b43f39573451ebc52c6915feb9beaf935c
Tom Flynn <tom.flynn@gmail.com>
2021-03-09 21:07:05 -0800

    Remove grpc generated c++ code from aci-containers repo
    The opflex repo generates the code itself from the same proto
    so there's no value in having these committed here
    
    Signed-off-by: Tom Flynn <tom.flynn@gmail.com>

--- pkg/gbpserver/proto/cxx/build_proto_cxx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run --rm -m 16g -v ${PWD}/..:/go/src/github.com/noironetworks/aci-containers/pkg/gbpserver/proto -w /go/src/github.com/noironetworks/aci-containers/pkg/gbpserver/proto --network=host -it grpc/cxx protoc -I . --grpc_out=./cxx --plugin=protoc-gen-grpc=/usr/local/bin/grpc_cpp_plugin ./gbp.proto
-docker run --rm -m 16g -v ${PWD}/..:/go/src/github.com/noironetworks/aci-containers/pkg/gbpserver/proto -w /go/src/github.com/noironetworks/aci-containers/pkg/gbpserver/proto --network=host -it grpc/cxx protoc -I . --cpp_out=./cxx ./gbp.proto
