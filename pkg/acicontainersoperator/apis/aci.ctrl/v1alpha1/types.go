6e7735f82fdfb1c5a27de5166adfd08b3494bf87
Apoorva <apomitta@apoorva-vm1.local.lan>
2021-03-26 19:40:08 -0700

    omit

--- pkg/acicontainersoperator/apis/aci.ctrl/v1alpha1/types.go
+++ pkg/acicontainersoperator/apis/aci.ctrl/v1alpha1/types.go
@@ -28,72 +28,72 @@ type AciContainersOperatorSpec struct {
 
 type AccProvisionSpec struct {
 	DeploymentOperator bool `json:"deployment_operator"`
-	AciConfig          AciConfigSpec `json:"aci_config"`
-	NetConfig          NetConfigSpec `json:"net_config"`
-	Registry           RegistrySpec `json:"registry"`
-	Logging            LoggingSpec `json:"logging"`
-	IstioConfig        IstioConfigSpec `json:"istio_config"`
-	DropLogConfig      DropLogConfigSpec `json:"drop_log_config"`
-	Multus             MultusSpec `json:"multus"`
-	KubeConfig         KubeConfigSpec `json:"kube_config"`
+	AciConfig          AciConfigSpec `json:"aci_config,omitempty"`
+	NetConfig          NetConfigSpec `json:"net_config,omitempty"`
+	Registry           RegistrySpec `json:"registry,omitempty"`
+	Logging            LoggingSpec `json:"logging,omitempty"`
+	IstioConfig        IstioConfigSpec `json:"istio_config,omitempty"`
+	DropLogConfig      DropLogConfigSpec `json:"drop_log_config,omitempty"`
+	Multus             MultusSpec `json:"multus,omitempty"`
+	KubeConfig         KubeConfigSpec `json:"kube_config,omitempty"`
 }
 
 type AciConfigSpec struct {
-	ClusterTenant string `json:"cluster_tenant"`
-	SyncLogin     SyncLoginSpec `json:"sync_login"`
-	ClientSSL     bool `json:"client_ssl"`
+	ClusterTenant string `json:"cluster_tenant,omitempty"`
+	SyncLogin     SyncLoginSpec `json:"sync_login,omitempty"`
+	ClientSSL     bool `json:"client_ssl,omitempty"`
 }
 
 type SyncLoginSpec struct {
-	CertFile string `json:"certfile"`
-	KeyFile  string `json:"keyfile"`
+	CertFile string `json:"certfile,omitempty"`
+	KeyFile  string `json:"keyfile,omitempty"`
 }
 
 type NetConfigSpec struct {
 	InterfaceMTU       int `json:"interface_mtu,omitempty"`
 	SvcMonitorInterval int `json:"service_monitor_interval,omitempty"`
-	PBRTrackingNonSNAT bool `json:"pbr_tracking_non_snat"`
+	PBRTrackingNonSNAT bool `json:"pbr_tracking_non_snat,omitempty"`
 }
 
 type RegistrySpec struct {
-	ImagePrefix                    string `json:"image_prefix"`
-	ImagePullSecret                string `json:"image_pull_secret"`
-        AccProvisionOperatorVersion    string `json:"acc_provision_operator_version"`
-	AciContainersControllerVersion string `json:"aci_containers_controller_version"`
-	AciContainersHostVersion       string `json:"aci_containers_host_version"`
-	CNIDeployVersion               string `json:"cnideploy_version"`
-	OpflexAgentVersion             string `json:"opflex_agent_version"`
-	OpenVSwitchVersion             string `json:"openvswitch_version"`
-	GBPVersion                     string `json:"gbp_version"`
+	ImagePrefix                    string `json:"image_prefix,omitempty"`
+	ImagePullSecret                string `json:"image_pull_secret,omitempty"`
+        AccProvisionOperatorVersion    string `json:"acc_provision_operator_version,omitempty"`
+	AciContainersControllerVersion string `json:"aci_containers_controller_version,omitempty"`
+	AciContainersHostVersion       string `json:"aci_containers_host_version,omitempty"`
+	CNIDeployVersion               string `json:"cnideploy_version,omitempty"`
+	OpflexAgentVersion             string `json:"opflex_agent_version,omitempty"`
+	OpenVSwitchVersion             string `json:"openvswitch_version,omitempty"`
+	GBPVersion                     string `json:"gbp_version,omitempty"`
 }
 
 type LoggingSpec struct {
-	ControllerLogLevel  string `json:"controller_log_level"`
-	HostagentLogLevel   string `json:"hostagent_log_level"`
-	OpflexagentLogLevel string `json:"opflexagent_log_level"`
+	ControllerLogLevel  string `json:"controller_log_level,omitempty"`
+	HostagentLogLevel   string `json:"hostagent_log_level,omitempty"`
+	OpflexagentLogLevel string `json:"opflexagent_log_level,omitempty"`
 }
 
 type IstioConfigSpec struct {
-	InstallIstio   bool `json:"install_istio"`
-	InstallProfile string `json:"install_profile"`
+	InstallIstio   bool `json:"install_istio,omitempty"`
+	InstallProfile string `json:"install_profile,omitempty"`
 }
 
 type MultusSpec struct {
-	Disable bool `json:"disable"`
+	Disable bool `json:"disable,omitempty"`
 }
 
 type DropLogConfigSpec struct {
-	Enable bool `json:"enable"`
+	Enable bool `json:"enable,omitempty"`
 }
 
 type KubeConfigSpec struct {
-	OVSMemoryLimit                         string `json:"ovs_memory_limit"`
-	UseExternalServiceIpAllocator          bool `json:"use_external_service_ip_allocator"`
-	UsePrivilegedContainers                bool `json:"use_privileged_containers"`
-	UseOpenshiftSecurityContextConstraints bool `json:"use_openshift_security_context_constraints"`
-	AllowKubeAPIDefaultEPG                 bool `json:"allow_kube_api_default_epg"`
-	ImagePullPolicy                        string `json:"image_pull_policy"`
-	RebootOpflexWithOvs                    string `json:"reboot_opflex_with_ovs"`
+	OVSMemoryLimit                         string `json:"ovs_memory_limit,omitempty"`
+	UseExternalServiceIpAllocator          bool `json:"use_external_service_ip_allocator,omitempty"`
+	UsePrivilegedContainers                bool `json:"use_privileged_containers,omitempty"`
+	UseOpenshiftSecurityContextConstraints bool `json:"use_openshift_security_context_constraints,omitempty"`
+	AllowKubeAPIDefaultEPG                 bool `json:"allow_kube_api_default_epg,omitempty"`
+	ImagePullPolicy                        string `json:"image_pull_policy,omitempty"`
+	RebootOpflexWithOvs                    string `json:"reboot_opflex_with_ovs,omitempty"`
 }
 
 // AciContainersOperatorStatus defines the observed state of AciContainersOperator
