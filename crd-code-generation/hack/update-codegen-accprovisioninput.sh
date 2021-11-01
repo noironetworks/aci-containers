758a4ce5397f2ea0690711f0e73d319e14c70681
vlella <lellavishal@gmail.com>
2021-11-01 13:49:10 -0700

    Revert "Corresponding changes in aci-containers as a followup to version name change in acc-provision"
    This reverts commit fb59093cf3c4632849310901651260b57584c1a8.

--- crd-code-generation/hack/update-codegen-accprovisioninput.sh
+++ crd-code-generation/hack/update-codegen-accprovisioninput.sh
@@ -8,5 +8,5 @@ SCRIPT_ROOT=$(dirname ${PWD})
 
 vendor/k8s.io/code-generator/generate-groups.sh all \
   github.com/noironetworks/aci-containers/pkg/accprovisioninput github.com/noironetworks/aci-containers/pkg/accprovisioninput/apis \
-  aci.ctrl:v1 \
+  aci.ctrl:v1alpha1 \
   --go-header-file ${SCRIPT_ROOT}/aci-containers/crd-code-generation/hack/custom-boilerplate.go.txt
