d1e2b536f084a215248a686af2d0d1a30a836dfa
Madhu Challa <challa@gmail.com>
2023-04-20 11:29:13 -0700

    Add suppport for ubi9-centos builds for opflex
    Signed-off-by: Madhu Challa <challa@gmail.com>
    (cherry picked from commit 9bb81b4589616fce15c0164be2b4c60b64a3cc63)

---
+++ docker/centos/launch-opflexagent.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+set -e
+set -x
+
+PREFIX=/usr/local
+VARDIR=${PREFIX}/var
+OPFLEXAGENT=${PREFIX}/bin/opflex_agent
+OPFLEXAGENT_CONF_PATH=/usr/local/etc/opflex-agent-ovs
+OPFLEXAGENT_REBOOT_CONFD=${VARDIR}/lib/opflex-agent-ovs/reboot-conf.d
+OPFLEXAGENT_DISABLED_CONF=${OPFLEXAGENT_CONF_PATH}/opflex-agent.conf
+OPFLEXAGENT_BASE_CONF=${OPFLEXAGENT_CONF_PATH}/base-conf.d
+OPFLEXAGENT_CONFD=${OPFLEXAGENT_CONF_PATH}/conf.d
+export LD_LIBRARY_PATH=/usr/local/lib64:$LD_LIBRARY_PATH
+
+if [ -w ${PREFIX} ]; then
+    # Setup folders to hold metadata
+    mkdir -p ${VARDIR}/lib/opflex-agent-ovs/endpoints
+    mkdir -p ${VARDIR}/lib/opflex-agent-ovs/services
+    mkdir -p ${VARDIR}/lib/opflex-agent-ovs/ids
+    mkdir -p ${VARDIR}/lib/opflex-agent-ovs/mcast
+    mkdir -p ${VARDIR}/lib/opflex-agent-ovs/snats
+    mkdir -p ${VARDIR}/lib/opflex-agent-ovs/reboot-conf.d
+    mkdir -p ${VARDIR}/lib/opflex-agent-ovs/droplog
+    mkdir -p ${VARDIR}/lib/opflex-agent-ovs/faults
+fi
+
+if [ -d ${OPFLEXAGENT_CONF_PATH} ]; then
+    cat <<EOF > ${OPFLEXAGENT_DISABLED_CONF}
+{
+    "opflex": {
+        "name": "disabled",
+        "domain": "disabled",
+        "ssl": {
+            "mode": "$SSL_MODE",
+            "ca-store": "/etc/ssl/certs/"
+        }
+    }
+}
+EOF
+fi
+
+if [ "$REBOOT_WITH_OVS" == "true" ]; then
+	exec ${OPFLEXAGENT} -w \
+		 -c ${OPFLEXAGENT_DISABLED_CONF} \
+		 -c ${OPFLEXAGENT_BASE_CONF} \
+		 -c ${OPFLEXAGENT_CONFD} \
+		 -c ${OPFLEXAGENT_REBOOT_CONFD}
+else
+	exec ${OPFLEXAGENT} -w \
+		 -c ${OPFLEXAGENT_DISABLED_CONF} \
+		 -c ${OPFLEXAGENT_BASE_CONF} \
+		 -c ${OPFLEXAGENT_CONFD}
+fi
