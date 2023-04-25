d19b4c754bf1daf96f9052f5ef206aa9d98f03a7
snaiksat <sumitnaiksatam@gmail.com>
2023-04-25 02:20:08 -0400

    Build opflex image in travis

---
+++ docker/centos/build-opflex-travis.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+set -x
+
+DOCKER_HUB_ID=quay.io/noirolabs
+DOCKER_TAG=sumit-kmr2-test
+
+SECOPT=
+export SECOPT
+
+DOCKER_DIR=docker/centos
+
+[ -z "$OPFLEX_DIR" ] && OPFLEX_DIR=/tmp/opflex
+export OPFLEX_DIR
+
+[ -z "$DOCKER_TAG" ] && DOCKER_TAG=
+export DOCKER_HUB_ID
+export DOCKER_TAG
+
+set -Eeuxo pipefail
+
+echo "starting opflex build"
+
+docker build $SECOPT -t $DOCKER_HUB_ID/opflex-build-base:$DOCKER_TAG -f $DOCKER_DIR/Dockerfile-opflex-build-base .
+#docker push $DOCKER_HUB_ID/opflex-build-base$DOCKER_TAG
+
+pushd $OPFLEX_DIR/genie
+mvn compile exec:java
+popd
+docker build $SECOPT -t $DOCKER_HUB_ID/opflex-build:$DOCKER_TAG -f $DOCKER_DIR/Dockerfile-opflex-build $OPFLEX_DIR
+#docker push $DOCKER_HUB_ID/opflex-build$DOCKER_TAG
+
+################## Copy everything from build into host ###############
+rm -Rf build/opflex/dist
+mkdir -p build/opflex/dist
+mkdir -p build/opflex/dist/agent
+mkdir -p build/opflex/dist/server
+mkdir -p build/opflex/dist/usr/local/lib64
+id=$(docker create $DOCKER_HUB_ID/opflex-build:$DOCKER_TAG)
+docker cp -L $id:/usr/local/lib64 build/opflex/dist/usr/local
+docker rm -v $id
+
+docker run $DOCKER_HUB_ID/opflex-build:$DOCKER_TAG tar -c -C /usr/local \
+    bin/opflex_agent bin/gbp_inspect bin/mcast_daemon bin/opflex_server \
+    | tar -x -C build/opflex/dist
+docker run -w /usr/local $DOCKER_HUB_ID/opflex-build:$DOCKER_TAG /bin/sh -c 'find lib \(\
+         -name '\''libopflex*.so*'\'' -o \
+         -name '\''libmodelgbp*so*'\'' -o \
+         -name '\''libopenvswitch*so*'\'' -o \
+         -name '\''libsflow*so*'\'' -o \
+         -name '\''libprometheus-cpp-*so*'\'' -o \
+         -name '\''libgrpc*so*'\'' -o \
+         -name '\''libproto*so*'\'' -o \
+         -name '\''libre2*so*'\'' -o \
+         -name '\''libupb*so*'\'' -o \
+         -name '\''libabsl*so*'\'' -o \
+         -name '\''libssl*so*'\'' -o \
+         -name '\''libcrypto*so*'\'' -o \
+         -name '\''libaddress_sorting*so*'\'' -o \
+         -name '\''libgpr*so*'\'' -o \
+         -name '\''libofproto*so*'\'' \
+         \) ! -name '\''*debug'\'' \
+        | xargs tar -c ' \
+    | tar -x -C build/opflex/dist
+docker run -w /usr/local $DOCKER_HUB_ID/opflex-build:$DOCKER_TAG /bin/sh -c 'find lib \(\
+         -name '\''libopflex*.so*'\'' -o \
+         -name '\''libmodelgbp*so*'\'' -o \
+         -name '\''libopenvswitch*so*'\'' -o \
+         -name '\''libsflow*so*'\'' -o \
+         -name '\''libprometheus-cpp-*so*'\'' -o \
+         -name '\''libofproto*so*'\'' \
+         \) ! -name '\''*debug'\'' \
+        | xargs tar -c ' \
+    | tar -x -C build/opflex/dist/agent
+docker run -w /usr/local $DOCKER_HUB_ID/opflex-build:$DOCKER_TAG /bin/sh -c 'find lib \(\
+         -name '\''libopflex*.so*'\'' -o \
+         -name '\''libmodelgbp*so*'\'' -o \
+         -name '\''libprometheus-cpp-*so*'\'' -o \
+         -name '\''libgrpc*so*'\'' -o \
+         -name '\''libproto*so*'\'' -o \
+         -name '\''libre2*so*'\'' -o \
+         -name '\''libupb*so*'\'' -o \
+         -name '\''libabsl*so*'\'' -o \
+         -name '\''libssl*so*'\'' -o \
+         -name '\''libcrypto*so*'\'' -o \
+         -name '\''libaddress_sorting*so*'\'' -o \
+         -name '\''libgpr*so*'\'' \
+         \) ! -name '\''*debug'\'' \
+        | xargs tar -c ' \
+    | tar -x -C build/opflex/dist/server
+docker run -w /usr/local $DOCKER_HUB_ID/opflex-build:$DOCKER_TAG /bin/sh -c \
+	'find lib bin -name '\''*.debug'\'' | xargs tar -cz' \
+	 > opflex-debuginfo.tar.gz
+cp $DOCKER_DIR/launch-opflexagent.sh build/opflex/dist/bin/
+cp $DOCKER_DIR/launch-mcastdaemon.sh build/opflex/dist/bin/
+cp $DOCKER_DIR/launch-opflexserver.sh build/opflex/dist/bin/
+cp $DOCKER_DIR/Dockerfile-opflex build/opflex/dist/
+cp $DOCKER_DIR/Dockerfile-opflexserver build/opflex/dist/
+cp $DOCKER_DIR/Dockerfile-opflex-distro build/opflex/dist/
+
+#######################################################################################
+docker build -t quay.io/noirolabs/opflex:sumit-kmr2-test -f ./build/opflex/dist/Dockerfile-opflex build/opflex/dist
+docker push quay.io/noirolabs/opflex:sumit-kmr2-test
